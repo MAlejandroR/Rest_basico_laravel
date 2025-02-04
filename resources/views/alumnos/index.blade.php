1b86bdbe59c9e9fae893839be974817bd0ad1484
MAlejandroR <manuelromeromiguel@gmail.com>
2025-02-04 07:57:56 +0100

    con sweet alert en delete

--- resources/views/alumnos/index.blade.php
+++ resources/views/alumnos/index.blade.php
@@ -47,10 +47,12 @@
 
                     </td>
                     <td><!-- Delete-->
-                        <form action="{{route("alumnos.destroy", $fila->id)}}" method="POST">
+                        <form onsubmit=event.preventDefault()
+                              id="formulario{{$fila->id}}"
+                              action="{{route("alumnos.destroy", $fila->id)}}" method="POST">
                             @method('DELETE')
                             @csrf
-                            <button type="submit">
+                            <button onclick="confirmDelete({{$fila->id}})">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hover:text-red-500 size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                         </svg>
@@ -76,6 +78,16 @@
     }
 
     },5000);
+function confirmDelete(id){
+    swal({
+        title:"¿Confirmas este borrado",
+        text:"Esta acción no se puede deshacer",
+        icon:"warning",
+        buttos:true
+    }).then ((ok)=>{
+        document.getElementById("formulario"+id).submit()
+    });
 
+}
 </script>
 </x-layouts.layout>
